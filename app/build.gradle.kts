2e07facad18ac085882202e10e3ec53a8c4661cf
toilacube <151115rubikcube@gmail.com>
2024-05-15 08:35:37 +0700

    add user profile activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.otaliastudios:zoomlayout:1.9.0")
+    implementation ("com.ashokvarma.android:bottom-navigation-bar:2.2.0")
 }
